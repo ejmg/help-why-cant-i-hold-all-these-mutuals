df4c00257c34a33f5b4c3f8ac3109f928fafd377
eg046613 <elias.garcia@cerner.com>
2018-07-03 19:40:01 -0500

    i'm sorry i forgot to add this the first time i'm sorry i'm sorry

---
+++ secret.py
@@ -0,0 +1,13 @@
+"""
+Insert the values you find at https://apps.twitter.com/ below in their
+corresponding variable **between** the single quotes for each.
+
+If you do not have your user credentials created yet, go to step 1 of this
+blog post which describes how to create these values:
+https://ageof.info/setting-up-twitter-bot-with-python-and-tweepy.html
+"""
+
+CONSUMER_TOKEN = ''
+CONSUMER_SECRET = ''
+ACCESS_TOKEN = ''
+ACCESS_SECRET = ''
